e6285d57429acede58ac9eb0630eccf5934b0746
mdallow97 <mikedallow@gmail.com>
2019-04-25 23:29:35 -0700

    Added comments for raft.py and updated README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # DistributedConsensus
 Implements a simplified version of the Raft algorithm
+
+parse.py:  parses all incoming client commands
+process.py:  process all commands coming from the leader or client:
+    AppendEntries()
+    ClientCommit()
+    RequestVote()
+    
+follower.py:  handles the follower class, threads for receiving and parsing commands and sending them to the leader
+
+candidate.py:  handles the candidate class, it is not fully implemented
+
+cluster.py: handles the ClusterNode class, which supports the node timeouts and election process
+
+raft.py: main program, finds addresses and port numbers
+
+log.txt: contains the entry log for whatever node is running there
--- raft.py
+++ raft.py
@@ -2,9 +2,11 @@ import cluster
 import socket
 import sys
 
+# Get local host name (IP)
 hostname = socket.gethostname()
 host = socket.gethostbyname(hostname)
 
+# Parse arguments
 if len(sys.argv) < 2:
     print("Formats: ")
     print("\tLeader: python raft.py <list_of_node_IP's>")
@@ -16,19 +18,22 @@ if len(sys.argv) == 2:
     node = cluster.ClusterNode(host, 0, "leader", sys.argv[1])
 
 elif len(sys.argv) == 3:
-    # can be either leader or follower command
-    # P1 <ip> <port> OR P1 <follower_ip> <follower_ip>
+    # can be either follower or leader command
+    # P1 <leader_ip> <port> OR P1 <follower_ip> <follower_ip>
     isPort = True
     for c in sys.argv[2]:
+        # If the argument contains a period, it is an IP address
         if c == '.':
             isPort = False
 
+    # If it contains a port, then it is a follower
     if isPort:
         node = cluster.ClusterNode(sys.argv[1], int(sys.argv[2]), "follower", [])
     else:
         followers = [sys.argv[1], sys.argv[2]]
         node = cluster.ClusterNode(host, 0, "leader", followers)
 
+# If it is none of the above, then it is a leader with a list of follower IPs
 else:
     followers = []
     for i in range(1,len(sys.argv)-1):
