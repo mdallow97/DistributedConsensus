28dff7e704eb9e8ad045258b1f93ffa81be57bf4
mdallow97 <mikedallow@gmail.com>
2019-04-26 13:38:56 -0700

    Added comments and cleaned up code. ClientCommit is still incorrect!

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 # process.py
 
-# used by leader and follower
+# Delegate processing of command to below functions
 def processCommand(command, cluster_node):
 
     if command.getCommand() == "ClientCommit":
@@ -18,6 +18,7 @@ def processCommand(command, cluster_node):
         # RequestVote(<term>, <id>)
         return RequestVote(command, cluster_node)
 
+# Returns Success! or Failed!
 def ClientCommit(command, cluster_node):
     # ClienCommit(!<var>) or ClientCommit(<var>, <value>)
     # Process parameters
@@ -26,12 +27,15 @@ def ClientCommit(command, cluster_node):
     file = open("log.txt", 'r+')
     contents = ""
 
+    # Copy file contents into a string
     for line in file:
         contents = contents + line
 
+    # Search and find <var>
     index = contents.find(params[0])
     var = contents[index:index+len(params[0])]
 
+    # Finds start and end index of <value>
     start_index = index+len(params[0])+1
     temp = contents[start_index:len(contents)]
 
@@ -47,6 +51,7 @@ def ClientCommit(command, cluster_node):
         self.cluster_node.current_commit = val
         new_contents = ""
 
+        # IS THIS PART WRONG??
         if index > -1:
 
             for i in range(0, len(contents)):
@@ -77,19 +82,23 @@ def ClientCommit(command, cluster_node):
 
     return "Success!"
 
+# Returns either value at index, or Success!/Failed!
 def AppendEntries(command, commit_index):
-# AppendEntries(<index>, !<var>) or AppendEntries(<index>, <var>, <value>)
+    # AppendEntries(<index>, !<var>) or AppendEntries(<index>, <var>, <value>)
     log = open("log.txt", 'r+')
     params = command.getParams()
-    assert commit_index == int(params[0])
-    contents = ""
 
-    i = 0
+    # Need to make sure that the commit index is the same as the followers
+    assert commit_index == int(params[0])
+
+    # Copy file contents into a string
+    contents = ""
     for line in log:
         contents = contents + line
-        i = i + 1
 
+    # If it should return a value at a specific <var>
     if command.shouldReturnVal():
+        # Find start and end indexes for <value>
         index = contents.find(params[1])
         var = contents[index:index+len(params[1])]
 
@@ -100,7 +109,9 @@ def AppendEntries(command, commit_index):
         val = contents[start_index:end_index]
 
         return val
+
     else:
+        # New value to be added to the beginning of the file
         # Edit string and write to the file
         new_contents = params[1] + ':' + params[2] + '\n'
 
